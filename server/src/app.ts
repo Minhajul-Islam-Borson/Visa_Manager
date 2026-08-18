17e9588c25e85e4779be8615a47d1e2e8129a2e5
Minhajul-Islam-Borson <minhajulborson@gamil.com>
2026-08-19 01:19:24 +0600

    Update for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,12 @@ import reportRoutes from "./routes/report.routes";
 
 const app = express();
 //middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://visa-manager-eight.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(errorHandler);
 
